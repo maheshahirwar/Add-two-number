4bb65d11fd286ce11b51bd8ae148dbaca96a80a7
Mahesh Ahirwar <69970376+maheshahirwar@users.noreply.github.com>
2021-02-24 18:24:58 +0530

    Add files via upload
    Add two number

---
+++ Add.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Add{
+    public static void main(String[]args){
+   int a,b,c;
+    a=sc.nextInt();
+    b=sc.nextInt();
+   c=a+b;
+   System.out.println(c);
+}
+
+}
